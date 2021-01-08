f7168950d511a9f440eca0f7487afffcc1f571fb
thomas_mary <thomas_mary@groupevsc.com>
2021-01-08 14:42:11 +0100

    card

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,6 +4,9 @@
     <rank v-if="finDePartie" :partieName="partieName"></rank>
     <boardOne v-if="$route.params.id == 1"></boardOne>
     <boardTwo v-if="$route.params.id == 2"></boardTwo>
+    <button class="button is-rounded is-primary" @click="alert">
+      Regardez, une arme !
+    </button>
   </div>
 </template>
 
@@ -13,6 +16,7 @@ import Rank from "../components/Rank.vue";
 import json from "../data/data.json";
 import BoardOne from "../components/BoardOne.vue";
 import BoardTwo from "../components/BoardTwo.vue";
+import WeaponDetailsVue from "../components/WeaponDetails.vue";
 export default {
   components: { Score, Rank, BoardOne, BoardTwo },
   name: "Game",
@@ -21,9 +25,18 @@ export default {
       nextPlayer: json.nextPlayer,
       partieName: json.partieName,
       finDePartie: json.finDePartie, // false to see ranking - true to see score
-      msg: "Partie en cours avec le plateau/parcours choisi"
     };
-  }
+  },
+  methods: {
+    alert() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: WeaponDetailsVue,
+        hasModalCard: true,
+        trapFocus: true,
+      });
+    },
+  },
 };
 </script>
 
