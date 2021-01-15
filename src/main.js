61ab1c08191e9a87b9a0db22e298d9283d5e4525
thomas_mary <thomas_mary@groupevsc.com>
2021-01-15 14:04:50 +0100

    ajout de firebase

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ import Buefy from "buefy";
 
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
+//import { auth } from './config/firebaseConfig'
 
 Vue.use(Buefy);
-
 Vue.config.productionTip = false;
 
 new Vue({
